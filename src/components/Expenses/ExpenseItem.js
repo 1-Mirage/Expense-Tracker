3056d6d74a19ceef3c779014dc463af0ef05ba39
1-Mirage <prasad.ashish162@gmail.com>
2021-05-12 11:08:25 +0530

    Organizing file

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,6 +1,6 @@
 import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.css';
-import Card from './Card';
+import Card from '../UI/Card';
 function ExpenseItem(props) /* props is object which gets data from App.js from custom element.The data is key value pair.*/
 {
     
