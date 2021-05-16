34ba91154ae2f8926ec2499bf05787a31d78b076
1-Mirage <prasad.ashish162@gmail.com>
2021-05-16 09:56:14 +0530

    Dyamic Updation of List

--- src/App.js
+++ src/App.js
@@ -2,39 +2,43 @@ import logo from './logo.svg';
 import './App.css';
 import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses'
+import { useState } from 'react';
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
-  const addExpenseHandler=(expense)=>{
-     console.log(expense);
-     console.log('App');
-  }
+  const [expenses,setExpenses]=useState(DUMMY_EXPENSES);
+  const addExpenseHandler=(expense)=>{    
+     setExpenses((prevExpenses)=>              /*prevExpense contains the screenshot of previous state of list*/
+     {
+       return [expense, ...prevExpenses];
+     });
+  };
 
 
   return (
     <div>
     
        <NewExpense onAddExpense={addExpenseHandler}/>
-        <Expenses item={expenses}/>
+        <Expenses items={expenses}/>
       
     </div>
   );
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,27 +1,22 @@
-import {useState} from 'react'
+
 import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.css';
 import Card from '../UI/Card';
 function ExpenseItem(props) /* props is object which gets data from App.js from custom element.The data is key value pair.*/
 {
-    const [title,setTitle]=useState(props.title); /*Updating the component, setTitle is an function, title is the value which stores the updated value*/
-    const clickHandler=() =>{
-        setTitle("updated!");  /* Assigning value*/
-        console.log("Clicked");
-    }
     
     return (
        <Card className="expense-item">
            <ExpenseDate date={props.date}/>                                { /*We just can't put anything between custom compenent*/ }
            <div className="expense-item__description">
                <h2>
-                   {title}
+                   {props.title}
                </h2>
                <div className="expense-item__price">
                 {props.amount}
                </div>
            </div>
-           <button onClick={clickHandler}>Button</button>             {/*adding eventlistner and adding state(react hook)*/}
+           
        </Card>
     );
 }
